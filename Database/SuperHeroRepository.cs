30b012b2e26124d12de6c3bcf1f3c86b79e8bc1f
Izaeria <135593611+Izaeria@users.noreply.github.com>
2024-02-06 11:26:08 +0100

    Details Page

--- Database/SuperHeroRepository.cs
+++ Database/SuperHeroRepository.cs
@@ -6,7 +6,7 @@ namespace SuperHeroSocialClubLight.Database
 	{
 		public static List<SuperHero> SuperHeros { get; set; } = new List<SuperHero>{
 
-			new SuperHero(1, "Superman", "Clark Kent", "Super strength", "flight"),
+			new SuperHero(1, "Superman", "Clark Kent", "Super strength", "Flight"),
 			new SuperHero(2, "Batman", "Bruce Wayne", "Intelligence", "Martial Arts", "Gadgets"),
 			new SuperHero(3, "Wonder Woman", "Diana Prince", "Super strength", "Flight", "Lasso of Truth"),
 			new SuperHero(4, "Spider-Man", "Peter Parker", "Wall-crawling", "Spider-Sense", "Web-slinging"),
