6524a265c448af29baad05a062d73d7df0f237c8
JosefMontanari <josef.montanari94@gmail.com>
2024-02-06 09:46:50 +0100

    add constructor to super hero and created superherorepository static list

---
+++ Database/SuperHeroRepository.cs
@@ -0,0 +1,17 @@
+﻿using SuperHeroSocialClubLight.Models;
+
+namespace SuperHeroSocialClubLight.Database
+{
+	public static class SuperHeroRepository
+	{
+		public static List<SuperHero> SuperHeros { get; set; } = new List<SuperHero>{
+
+			new SuperHero(1, "Superman", "Clark Kent", "Super strength", "flight"),
+			new SuperHero(2, "Batman", "Bruce Wayne", "Intelligence", "Martial Arts", "Gadgets"),
+			new SuperHero(3, "Wonder Woman", "Diana Prince", "Super strength", "Flight", "Lasso of Truth"),
+			new SuperHero(4, "Spider-Man", "Peter Parker", "Wall-crawling", "Spider-Sense", "Web-slinging"),
+			new SuperHero(5, "Captain America", "Steve Rogers", "Super strength", "Shield mastery", "Peak human agility")
+
+			};
+	}
+}
--- Models/SuperHero.cs
+++ Models/SuperHero.cs
@@ -2,5 +2,20 @@
 {
 	public class SuperHero
 	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string? SecretIdentity { get; set; }
+		public List<string> SuperPowers { get; set; }
+		public string? Img { get; set; }
+
+		public SuperHero(int id, string name, string secretIdentity, params string[] superPowers)
+		{
+			Id = id;
+			Name = name;
+			SecretIdentity = secretIdentity;
+			SuperPowers = new List<string>(superPowers);
+			Img = Name + ".jpg";
+		}
+
 	}
 }
