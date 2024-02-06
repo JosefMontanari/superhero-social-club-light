5d5cc70a2761bee07eb9bda094299d7ab0d93fee
Izaeria <135593611+Izaeria@users.noreply.github.com>
2024-02-06 09:11:36 +0100

    added pages

---
+++ Pages/Shared/Superhero/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SuperHeroSocialClubLight.Pages.Shared.Superhero
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
