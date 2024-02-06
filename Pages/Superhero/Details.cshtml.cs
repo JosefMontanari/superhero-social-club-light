93db94c1b2907db8f444b9af1e0886f70dea6497
Izaeria <135593611+Izaeria@users.noreply.github.com>
2024-02-06 10:40:54 +0100

    Added superhero and details page

---
+++ Pages/Superhero/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SuperHeroSocialClubLight.Pages.Superhero
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Pages/Superhero/Superheroes.cshtml.cs
+++ Pages/Superhero/Superheroes.cshtml.cs
@@ -1,12 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperHeroSocialClubLight.Database;
+using SuperHeroSocialClubLight.Models;
 
 namespace SuperHeroSocialClubLight.Pages.Superhero
 {
-    public class SuperheroesModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class SuperheroesModel : PageModel
+	{
+		public List<SuperHero> Superheroes { get; set; }
+
+
+		public void OnGet()
+		{
+			Superheroes = SuperHeroRepository.SuperHeros;
+		}
+	}
 }
