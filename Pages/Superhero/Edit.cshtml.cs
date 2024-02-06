c57d0cf32de23669bcdba44c6812a908e760d88a
JosefMontanari <josef.montanari94@gmail.com>
2024-02-06 11:35:07 +0100

    finished edit function

--- Pages/Superhero/Edit.cshtml.cs
+++ Pages/Superhero/Edit.cshtml.cs
@@ -1,22 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperHeroSocialClubLight.Database;
+using SuperHeroSocialClubLight.Models;
 
 namespace SuperHeroSocialClubLight.Pages.Superhero
 {
-	[BindProperties]
+
 	public class EditModel : PageModel
 	{
+		public int Id { get; set; }
+		[BindProperty]
 		public string Name { get; set; }
+		[BindProperty]
 		public string SecretIdentity { get; set; }
-		public List<string> SuperPowers { get; set; }
+		[BindProperty]
+		public List<string>? SuperPowers { get; set; }
 
 
-		public void OnGet()
+		public void OnGet(int id)
 		{
+			Id = id;
 		}
 
 		public void OnPost()
 		{
+			int id = SuperHeroRepository.SuperHeros.Count + 1;
+			SuperHero superHero = new SuperHero(id, Name, SecretIdentity, SuperPowers.ToString());
+			SuperHeroRepository superHeroRepository = new SuperHeroRepository();
+			superHeroRepository.UpdateSuperHero(Id, superHero);
 		}
 	}
 }
