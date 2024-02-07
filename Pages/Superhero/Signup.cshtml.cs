d1c21044047a62dc7febea42dbc434fd93efe0eb
JosefMontanari <josef.montanari94@gmail.com>
2024-02-07 10:23:53 +0100

    Build signup page

--- Pages/Superhero/Signup.cshtml.cs
+++ Pages/Superhero/Signup.cshtml.cs
@@ -1,30 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperHeroSocialClubLight.Database;
+using SuperHeroSocialClubLight.Models;
 
 namespace SuperHeroSocialClubLight.Pages.Superhero
 {
 	[BindProperties]
 	public class SignupModel : PageModel
 	{
+
+		[BindProperty]
 		public string? Name { get; set; }
+		[BindProperty]
 		public string? SecretIdentity { get; set; }
-		public string? Superpower { get; set; }
+
+		[BindProperty]
+		public List<string>? SuperPowers { get; set; }
 
 
 		public void OnGet()
 		{
 		}
 
-		public IActionResult OnPost()
+		public void OnPost()
 		{
-			if (Name != null && SecretIdentity != null)
-			{
-				return RedirectToPage("Superhero/Superheroes");
-			}
-			else
-			{
-				return Page();
-			}
+			int id = SuperHeroRepository.SuperHeros.Count + 1;
+			SuperHero hero = new SuperHero(id, Name, SecretIdentity);
+			SuperHeroRepository.AddSuperPowers(SuperPowers, hero);
+			SuperHeroRepository.AddSuperHero(hero);
 		}
 
 	}
